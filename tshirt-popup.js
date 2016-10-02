715952d47abc5ae5504b759b425a7ab918424faf
Januar Siregar <januar.siregar@xinix.co.id>
2016-10-02 12:48:57 +0700

    fixng for change url on the fly

--- tshirt-popup.js
+++ tshirt-popup.js
@@ -13,10 +13,12 @@
 				iconPrefix: "xn",
 				width: "auto",
 				height: "auto",
+				youtube_id: null,
 				closeButton: true,
 				closeOutside: true,
 				disableTouchScroll: true,
-				onClick: function () {}
+				onClick: function (){},
+				onClose: function(){}
 			},
 			popup = this,
 			settings = $.extend({}, defaults, options);
@@ -24,83 +26,86 @@
 		this.close = function () {
 			$("#" + settings.id).fadeOut(128);
 			$("#" + settings.id + " > div > div > div").removeClass("animated " + settings.animation).html("");
+			settings.onClose();
 		};
 
 		return this.each(function () {
 			var elem = $(this),
 				url = elem.attr("href"),
 				youtube_id = "";
-
+				elem.settings  = settings;
+				elem.settings.url = url;
+				
 			// If url is predefine
-			if (settings.url !== "") {
-				url = settings.url;
-			}
-
-			if (url !== undefined) {
+			if (elem.settings.url !== undefined) {
 				// See if it's a youtube link
-				youtube_id = url.match(/(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/);
+				youtube_id = elem.settings.url.match(/(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/);
 
-				console.log ("1", youtube_id);
 
 				// Just to make sure that the url have youtube's domains
-				if (url.indexOf("youtube.com") <= -1 && url.indexOf("youtu.be") <= -1) {
-					youtube_id = null;
+				if (elem.settings.url.indexOf("youtube.com") <= -1 && elem.settings.url.indexOf("youtu.be") <= -1) {
+					elem.settings.youtube_id = null;
 				}
 
-				console.log ("2", url.indexOf("youtube.com") <= -1, url.indexOf("youtu.be") <= -1, youtube_id);
-
+				
 				if (youtube_id !== null) {
-					settings.type = "iframe";
+					elem.settings.type = "iframe";
 				}
 
 				// If target is defined
 				if (settings.target !== "") {
-					url = url + " " + settings.target;
+					elem.settings.url = elem.settings.url + " " + elem.settings.target;
 				}
 			}
 
 			elem.click(function (e) {
+
+				if(elem.attr('href')){
+					elem.settings.url = elem.attr('href');
+				}
+				
 				// Append all the necessary divs
 				if ($("#popupOverflow").length === 0) {
-					$("body").append("<div id='" + settings.id + "' style='display:none'><div id='popupTable'><div id='popupMargin'></div></div></div>");
+					$("body").append("<div id='" + elem.settings.id + "' style='display:none'><div id='popupTable'><div id='popupMargin'></div></div></div>");
 				}
 
 				// Add loading content
-				$("#" + settings.id + " > div > div").html("<div id='popupContent' class='" + settings.className + "'><div class='content'><p class='loading'><i class='" + settings.iconPrefix +" " + settings.iconPrefix +"-circle-o-notch " + settings.iconPrefix +"-spin " + settings.iconPrefix +"-5x'></i></p></div></div>");
+				$("#" + elem.settings.id + " > div > div").html("<div id='popupContent' class='" + elem.settings.className + "'><div class='content'><p class='loading'><i class='" + elem.settings.iconPrefix +" " + elem.settings.iconPrefix +"-circle-o-notch " + elem.settings.iconPrefix +"-spin " + elem.settings.iconPrefix +"-5x'></i></p></div></div>");
 
 				// Set Height and Width
-				if (settings.width != "auto") {
-					$("#" + settings.id + " > div > div > div > div").width (settings.width);
+				if (elem.settings.width != "auto") {
+					$("#" + elem.settings.id + " > div > div > div > div").width (elem.settings.width);
 				}
-				if (settings.height != "auto") {
-					$("#" + settings.id + " > div > div > div > div").height (settings.height);
+				if (elem.settings.height != "auto") {
+					$("#" + elem.settings.id + " > div > div > div > div").height (elem.settings.height);
 				}
 
 				// Draw close button
-				if (settings.closeButton) {
-					$("#" + settings.id + " > div > div > div").append("<a href='#' class='close'><i class='" + settings.iconPrefix +" " + settings.iconPrefix +"-close'></i></a>");
+				if (elem.settings.closeButton) {
+					$("#" + elem.settings.id + " > div > div > div").append("<a href='#' class='close'><i class='" + elem.settings.iconPrefix +" " + elem.settings.iconPrefix +"-close'></i></a>");
 				} else {
-					$("#" + settings.id + " .close").remove();
+					$("#" + elem.settings.id + " .close").remove();
 				}
 
-				// Display popup and prevent touch scroll event
-				$("#" + settings.id).fadeIn(100).on('touchmove', function(e) {
-					if (settings.disableTouchScroll) {
+				
+				// // Display popup and prevent touch scroll event
+				$("#" + elem.settings.id).fadeIn(100).on('touchmove', function(e) {
+					if (elem.settings.disableTouchScroll) {
 						e.preventDefault();
 					}
 				});
 
-				// Assign close action to close button
-				if (settings.closeButton) {
-					$("#" + settings.id + " .close").click(function (e) {
+				// // Assign close action to close button
+				if (elem.settings.closeButton) {
+					$("#" + elem.settings.id + " .close").click(function (e) {
 						popup.close();
 						e.preventDefault();
 					});
 				}
 
 				// Assign close action to shade area
-				if (settings.closeOutside) {
-					$("#" + settings.id + " > div > div").click(function (e) {
+				if (elem.settings.closeOutside) {
+					$("#" + elem.settings.id + " > div > div").click(function (e) {
 						if (e.target === this) {
 							popup.close();
 							e.preventDefault();
@@ -108,49 +113,50 @@
 					});
 				}
 
-				// Type of the popup is inline
-				if (settings.type === "inline") {
-					if (url !== undefined) {
-						// Ajax call
-						$("#" + settings.id + " > div > div > div > div").load( url, function( response, status, xhr ) {
+				// // Type of the popup is inline
+				if (elem.settings.type === "inline") {
+				
+					if (elem.settings.url !== undefined) {
+				// 		// Ajax call
+						$("#" + elem.settings.id + " > div > div > div > div").load( elem.settings.url, function( response, status, xhr ) {
 							// Ir url is not found or connection is broken
 							if ( status === "error" ) {
 								if ($("#popupOverflow .close").length === 0) {
-									$("#" + settings.id + " > div > div > div").append("<a href='#' class='close'><i class='" + settings.iconPrefix +" " + settings.iconPrefix +"-close'></i></a>");
-									$("#" + settings.id + " .close").click(function (e) {
+									$("#" + elem.settings.id + " > div > div > div").append("<a href='#' class='close'><i class='" + elem.settings.iconPrefix +" " + elem.settings.iconPrefix +"-close'></i></a>");
+									$("#" + elem.settings.id + " .close").click(function (e) {
 										popup.close();
 										e.preventDefault();
 									});
 								}
 
-								$("#" + settings.id + " > div > div > div").addClass("animated " + settings.animation)
-								$("#" + settings.id + " > div > div > div > div").html("<p class='error'><i class='" + settings.iconPrefix +" " + settings.iconPrefix +"-rocket " + settings.iconPrefix +"-5x'></i><br />Error while loading your content<br />Please close this popup and try again</p>");
+								$("#" + elem.settings.id + " > div > div > div").addClass("animated " + elem.settings.animation)
+								$("#" + elem.settings.id + " > div > div > div > div").html("<p class='error'><i class='" + elem.settings.iconPrefix +" " + elem.settings.iconPrefix +"-rocket " + elem.settings.iconPrefix +"-5x'></i><br />Error while loading your content<br />Please close this popup and try again</p>");
 							}
 						});
-					} else if (settings.target !== "") {
-						$("#" + settings.id + " > div > div > div > div").html($(settings.target).html ());
+					} else if (elem.settings.target !== "") {
+						$("#" + elem.settings.id + " > div > div > div > div").html($(elem.settings.target).html ());
 					}
 
 
-				// Type of the popup is iframe
-				} else if (settings.type === "iframe") {
+				// // Type of the popup is iframe
+				} else if (elem.settings.type === "iframe") {
 					if (youtube_id !== null) {
-						$("#" + settings.id + " > div > div > div > div").addClass("youtube").html ('<iframe width="800" height="450" src="https://www.youtube.com/embed/' + youtube_id[1] + '" frameborder="0" allowfullscreen></iframe>');
+						$("#" + elem.settings.id + " > div > div > div > div").addClass("youtube").html ('<iframe width="800" height="450" src="https://www.youtube.com/embed/' + youtube_id[1] + '" frameborder="0" allowfullscreen></iframe>');
 					} else {
-						$("#" + settings.id + " > div > div > div > div").html ("<iframe src='" + url + "' width='100%' height='100%' frameborder='0' />");
+						$("#" + elem.settings.id + " > div > div > div > div").html ("<iframe src='" + url + "' width='100%' height='100%' frameborder='0' />");
 					}
 				}
 
-				// Display or not close button
-				if (!settings.closeButton) {
-					$("#" + settings.id + " .close").remove();
+				// // Display or not close button
+				if (!elem.settings.closeButton) {
+					$("#" + elem.settings.id + " .close").remove();
 				}
 
-				// Add animation
-				$("#" + settings.id + " > div > div > div").addClass("animated " + settings.animation);
+				// // Add animation
+				$("#" + elem.settings.id + " > div > div > div").addClass("animated " + elem.settings.animation);
 
-				settings.onClick ();
-
+				elem.settings.onClick ();
+				
 				e.preventDefault();
 			});
 		});
